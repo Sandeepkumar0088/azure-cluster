c62cc71431ee3a4274d47808932c0d3afe267eb2
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-09-08 12:27:02 +0530

    azure cluster

--- cluster.tf
+++ cluster.tf
@@ -1,33 +1,3 @@
-# resource "azurerm_kubernetes_cluster" "dev" {
-#   name                = "dev"
-#   location            = azurerm_resource_group.cluster.location
-#   resource_group_name = azurerm_resource_group.cluster.name
-#   dns_prefix          = "dev"
-#
-#   default_node_pool {
-#     name           = "system"
-#     node_count     = 4
-#     vm_size        = "Standard_D4ls_v6"
-#     vnet_subnet_id = azurerm_subnet.aks.id
-#     os_sku         = "AzureLinux3"
-#   }
-#
-#   node_provisioning_profile {
-#     mode = "Manual"
-#   }
-#
-#   identity {
-#     type = "SystemAssigned"
-#   }
-#
-#   network_profile {
-#     network_plugin = "azure"
-#
-#     service_cidr   = "10.10.0.0/16"
-#     dns_service_ip = "10.10.0.10"
-#   }
-# }
-
 resource "azurerm_kubernetes_cluster" "dev" {
   name                = "dev"
   location            = azurerm_resource_group.cluster.location
