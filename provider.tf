126ee87e87d44882e1406f162673348150146073
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-09-09 11:21:55 +0530

    azure cluster

--- provider.tf
+++ provider.tf
@@ -2,15 +2,18 @@ provider "azurerm" {
   features {}
 }
 
-
-provider "helm" {
-  kubernetes = {
-    config_path = "~/.kube/config"
-  }
-}
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.dev.kube_config[0].host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.dev.kube_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.dev.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.dev.kube_config[0].cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = azurerm_kubernetes_cluster.dev.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.dev.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.dev.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.dev.kube_config[0].cluster_ca_certificate)
+  }
 }
