ec6a6cbb3fa2c3a42108ab5de7e7b78833f1172c
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-09-13 10:53:12 +0530

    azure cluster

--- cluster.tf
+++ cluster.tf
@@ -1,31 +1,3 @@
-# resource "azurerm_kubernetes_cluster" "dev" {
-#   name                = "dev"
-#   location            = azurerm_resource_group.cluster.location
-#   resource_group_name = azurerm_resource_group.cluster.name
-#   dns_prefix          = "dev"
-#
-#   default_node_pool {
-#     name           = "system"
-#     node_count     = 1
-#     vm_size        = "Standard_D2s_v6"
-#     vnet_subnet_id = azurerm_subnet.aks.id
-#     os_sku         = "AzureLinux3"
-#   }
-#   node_provisioning_profile {
-#     mode = "Manual"
-#   }
-#
-#   identity {
-#     type = "SystemAssigned"
-#   }
-#
-#   network_profile {
-#     network_plugin = "azure"
-#
-#     service_cidr   = "10.10.0.0/16"
-#     dns_service_ip = "10.10.0.10"
-#   }
-# }
 resource "azurerm_kubernetes_cluster" "dev" {
   name                = "dev"
   location            = azurerm_resource_group.cluster.location
@@ -93,20 +65,11 @@ resource "null_resource" "kubeconfig" {
     cluster = timestamp()
   }
 
-  # provisioner "local-exec" {
-  #   command = <<-EOT
-  #     sudo mv /tmp/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
-  #     sudo dnf install -y kubectl
-  #
-  #     kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-  #   EOT
-  # }
 }
 
 resource "helm_release" "nginx-ingress" {
 
   depends_on = [
-    null_resource.jenkins,
     null_resource.kubeconfig
   ]
 
@@ -142,15 +105,9 @@ resource "helm_release" "nginx-ingress" {
   ]
 }
 
-#managed identity
-# resource "azurerm_user_assigned_identity" "external_dns" {
-#   name                = "managed-identity"
-#   location            = azurerm_resource_group.cluster.location
-#   resource_group_name = azurerm_resource_group.cluster.name
-# }
 data "azurerm_user_assigned_identity" "external_dns" {
   name                = "dns_identity"
-  resource_group_name = "dns"
+  resource_group_name = "azure"
 }
 # workload identity
 resource "azurerm_federated_identity_credential" "external_dns" {
@@ -186,95 +143,7 @@ resource "kubernetes_service_account_v1" "external_dns" {
   ]
 }
 
-# resource "helm_release" "external_dns" {
-#   name       = "external-dns"
-#   repository = "https://kubernetes-sigs.github.io/external-dns/"
-#   chart      = "external-dns"
-#   namespace  = var.external_dns_namespace
-#
-#   create_namespace = false
-#
-#   values = [
-#     yamlencode({
-#       fullnameOverride = "external-dns"
-#
-#       provider = {
-#         name = "azure"
-#       }
-#
-#       serviceAccount = {
-#         create = false
-#         name   = var.external_dns_service_account
-#       }
-#
-#       sources = [
-#         "ingress"
-#       ]
-#
-#       domainFilters = [
-#         var.dns_zone_name
-#       ]
-#
-#       policy = "upsert-only"
-#
-#       registry = "txt"
-#
-#       txtOwnerId = "dev-external-dns"
-#
-#       podLabels = {
-#         "azure.workload.identity/use" = "true"
-#       }
-#     })
-#   ]
-#
-#   depends_on = [
-#     kubernetes_service_account_v1.external_dns,
-#     azurerm_federated_identity_credential.external_dns
-#   ]
-# }
 
-# resource "helm_release" "external_dns" {
-#   name             = "external-dns"
-#   repository       = "https://kubernetes-sigs.github.io/external-dns/"
-#   chart            = "external-dns"
-#   namespace        = var.external_dns_namespace
-#   create_namespace = false
-#
-#   values = [
-#     yamlencode({
-#       fullnameOverride = "external-dns"
-#
-#       provider = {
-#         name = "azure"
-#       }
-#
-#       serviceAccount = {
-#         create = false
-#         name   = var.external_dns_service_account
-#       }
-#
-#       sources = ["ingress"]
-#
-#       domainFilters = [
-#         var.dns_zone_name
-#       ]
-#
-#       policy = "upsert-only"
-#
-#       registry = "txt"
-#
-#       txtOwnerId = "dev-external-dns"
-#
-#       podLabels = {
-#         "azure.workload.identity/use" = "true"
-#       }
-#     })
-#   ]
-#
-#   depends_on = [
-#     kubernetes_service_account_v1.external_dns
-#   ]
-# }
 data "azurerm_client_config" "current" {}
 
 resource "kubernetes_secret_v1" "external_dns_azure" {
@@ -359,4 +228,34 @@ resource "helm_release" "external_dns" {
     kubernetes_service_account_v1.external_dns,
     kubernetes_secret_v1.external_dns_azure,
   ]
+}
+resource "helm_release" "prometheus-stack" {
+  depends_on  = [ null_resource.kubeconfig, helm_release.external_dns,helm_release.nginx-ingress  ]
+
+  name        = "promstack"
+  repository  = "https://prometheus-community.github.io/helm-charts"
+  chart       = "kube-prometheus-stack"
+  values      = [file("prom-stack-values.yml")]
+
+  set = [
+    {
+      name  = "grafana.enabled"
+      value = "false"
+    },
+    {
+      name  = "prometheus.ingress.enabled"
+      value = "true"
+    },
+    {
+      name  = "prometheus.ingress.ingressClassName"
+      value = "nginx"
+    }
+  ]
+  set_list = [
+    {
+      name  = "prometheus.ingress.hosts"
+      value = ["prometheus-dev.sandeepkumarpenta.online"]
+    }
+  ]
+
 }
